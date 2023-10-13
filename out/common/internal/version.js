72879206e786811115b19da3436b5efdd948e83d
jzm-intel <zhaoming.jiang@intel.com>
2023-10-13 09:55:17 +0000

    Deploying to gh-pages from @ jzm-intel/cts@56e1a2e473ce150fe22e29a5b8788a8be3938cca 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '8bb48275486acc5ce8ec25dcd3eabaeba80ed832';
+export const version = '56e1a2e473ce150fe22e29a5b8788a8be3938cca';
